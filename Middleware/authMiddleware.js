fdc5907104929127c4300b5c0cd5d6af4d924c3a
Naruto Reza <mtreza59@gmail.com>
2024-09-03 17:18:41 +0530

    0.0.1

---
+++ Middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next){
+    const accessToken = req.header('Authorization');
+    const refreshToken = req.body.refreshToken;
+
+    try{
+        if(!accessToken){
+            if(!refreshToken) return res.json('Access Denied.');
+            else{
+                res.json("Access needed!")
+            }
+        }
+        else{
+            if(typeof token !== undefined){
+                const bearerToken = accessToken.split(" ")[1];
+                const decoded = jwt.verify(bearerToken, process.env.secret_token);
+                req.userId = decoded.userId;
+                next();
+            }
+            else{
+                res.json({
+                    status: 'fail',
+                    message: 'Unauthorized!',
+                });
+            }
+        }
+    }
+    catch(err) {
+        res.json('Access Denied.');
+    }
+}
+
+module.exports = verifyToken;
